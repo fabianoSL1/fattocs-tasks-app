c94f01d0564bed6233013ff3e45776a84be934e3
Fabiano Lima <fabianosl@ufrrj.br>
2024-11-01 00:17:03 -0300

    style(controller): padronizando export de task controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,17 @@
 import { Hono } from "hono";
-import TaskController from "./infra/controllers/TaskController";
 import { HTTPException } from "hono/http-exception";
+import { taskController } from "./infra/controllers/TaskController";
 
 export const app = new Hono();
 
-app.route("/tasks", TaskController);
+app.route("/tasks", taskController);
 
 app.onError((err, ctx) => {
     if (err instanceof HTTPException) {
         return ctx.json({ message: err.cause }, err.status);
     }
 
+    console.error(err);
+    
     return ctx.text("internal server error", 500);
 });
--- backend/src/infra/controllers/TaskController.ts
+++ backend/src/infra/controllers/TaskController.ts
@@ -16,20 +16,19 @@ import {
     ReOrderTaskRequest,
     reOrderTaskSchema,
 } from "../../dto";
-import { TaskRepository } from "../../core/TaskRepository";
-import { MemoryTaskRepository } from "../repository/MemoryTaskRepository";
+import { TaskRepositoryFactory } from "../repository/index";
 
-const route = new Hono();
-const taskRepository: TaskRepository = new MemoryTaskRepository();
+const taskRepository = TaskRepositoryFactory();
+export const taskController = new Hono();
 
-route.get("/", async (ctx) => {
+taskController.get("/", async (ctx) => {
     const listTasksUseCase = new ListTasksUseCase(taskRepository);
     const tasks = await listTasksUseCase.execute();
 
     return ctx.json(tasks);
 });
 
-route.post("/", zValidator("json", createTaskSchema), async (ctx) => {
+taskController.post("/", zValidator("json", createTaskSchema), async (ctx) => {
     const request: CreateTaskRequest = ctx.req.valid("json");
     const createTaskUseCase = new CreateTaskUseCase(taskRepository);
     const task = await createTaskUseCase.execute(request);
@@ -37,32 +36,38 @@ route.post("/", zValidator("json", createTaskSchema), async (ctx) => {
     return ctx.json(task);
 });
 
-route.patch("/:id", zValidator("json", editTaskSchema), async (ctx) => {
-    const { id } = ctx.req.param();
-    const request: EditTaskRequest = ctx.req.valid("json");
+taskController.patch(
+    "/:id",
+    zValidator("json", editTaskSchema),
+    async (ctx) => {
+        const { id } = ctx.req.param();
+        const request: EditTaskRequest = ctx.req.valid("json");
 
-    const editTaskUseCase = new EditTaskUseCase(taskRepository);
-    const task = await editTaskUseCase.execute(id, request);
+        const editTaskUseCase = new EditTaskUseCase(taskRepository);
+        const task = await editTaskUseCase.execute(id, request);
 
-    return ctx.json(task);
-});
+        return ctx.json(task);
+    },
+);
 
-route.patch("/:id/order", zValidator("json", reOrderTaskSchema), async (ctx) => {
-    const { id } = ctx.req.param();
-    const request: ReOrderTaskRequest = ctx.req.valid("json");
+taskController.patch(
+    "/:id/order",
+    zValidator("json", reOrderTaskSchema),
+    async (ctx) => {
+        const { id } = ctx.req.param();
+        const request: ReOrderTaskRequest = ctx.req.valid("json");
 
-    const reOrderTaskUseCase = new ReOrderTaskUseCase(taskRepository);
-    await reOrderTaskUseCase.execute(id, request);
+        const reOrderTaskUseCase = new ReOrderTaskUseCase(taskRepository);
+        await reOrderTaskUseCase.execute(id, request);
 
-    return ctx.json({});
-});
+        return ctx.json({});
+    },
+);
 
-route.delete("/:id", async (ctx) => {
+taskController.delete("/:id", async (ctx) => {
     const { id } = ctx.req.param();
     const deleteTaskUseCase = new DeleteTaskUseCase(taskRepository);
     const task = await deleteTaskUseCase.execute(id);
 
     return ctx.json(task);
 });
-
-export default route;
